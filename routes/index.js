9436fd1726521c04b7baa60eebf822a12fba1969
panforrest <guoqianp@gmail.com>
2018-02-07 22:22:35 -0500

    index template with data: Welcome to my restaurant

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+// Full Documentation - https://www.turbo360.co/docs
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+/*  This is the home route. It renders the index.mustache page from the views directory.
+	Data is rendered using the Mustache templating engine. For more
+	information, view here: https://mustache.github.io/#demo */
+router.get('/', function(req, res){
+    const data = {
+    	greeting: 'Welcome to my restaurant'
+    }
+
+	res.render('index', data)
+})
+
+module.exports = router
