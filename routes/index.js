b6d9f97b96b34b1079109f6e9bc548f15d817086
panforrest <guoqianp@gmail.com>
2018-02-17 00:06:31 -0500

    end of V2 global env variable and image process

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = vertex.router()
 	information, view here: https://mustache.github.io/#demo */
 router.get('/', function(req, res){
     const data = {
-    	cdn: 'https://cdn.turbo360-vertex.com/base-restaurant-lw9cxv/public',
+    	cdn: process.env.TURBO_CDN,
     	greeting: 'Welcome to my restaurant',
     	description: 'This is a place for business meeting'
     }
